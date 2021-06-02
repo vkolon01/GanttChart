9af42c6c13fd50da04804290d5ea9848cb3c89e9
Vitali Kolontko <vkolon01@mail.bbk.ac.uk>
2021-06-02 08:49:44 +0100

    added docker related files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {version} from '../../package.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'grant-spike';
+
+  version: string = '';
+
+  ngOnInit() {
+    this.version = version;
+  }
 }
