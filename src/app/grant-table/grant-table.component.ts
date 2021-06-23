e7dae2d918ee831cb22776e1c83f451f841fd24d
Vitali Kolontko <vkolon01@mail.bbk.ac.uk>
2021-06-23 11:52:46 +0100

    made table rows reactive

--- src/app/grant-table/grant-table.component.ts
+++ src/app/grant-table/grant-table.component.ts
@@ -1,5 +1,7 @@
 import {AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 import {interval} from 'rxjs';
+import {GanttBar} from "../gantt-bar";
+import {Form, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-grant-table',
@@ -21,6 +23,23 @@ export class GrantTableComponent implements OnInit, AfterViewInit {
   scrollTable = false;
 
   zoomIntensity = 0.1;
+
+
+
+  chartElementMatrix: Array<Array<GanttBar>> = [
+    [
+      {start: 0, width: 10, fillColor: 'red'},
+      {start: 20, width: 5, fillColor: 'blue'},
+      {start: 28, width: 10, fillColor: 'black', strokeColor: 'purple'},
+      {start: 40, width: 10, fillColor: 'yellow'}
+    ],
+    [
+      {start: 5, width: 3, fillColor: 'red'},
+      {start: 12, width: 5, fillColor: 'blue'},
+      {start: 18, width: 10, fillColor: 'black', strokeColor: 'purple'},
+      {start: 26, width: 3, fillColor: 'yellow'}
+    ]
+  ];
 
   // config
   smooth = true;
@@ -66,6 +85,29 @@ export class GrantTableComponent implements OnInit, AfterViewInit {
     this.gridCanvasContext = this.gridCanvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
   }
 
+  // createTableForm(): FormGroup {
+  //
+  //   this.chartElementMatrix.forEach(row => {
+  //     const charts = [];
+  //
+  //     row.forEach(chart => {
+  //       const controls = [];
+  //       Object.keys(chart).forEach(key => {
+  //         controls[key] = new FormControl(charts[key])
+  //       })
+  //       charts.push(new FormGroup([
+  //
+  //       ]))
+  //     })
+  //   })
+  //
+  //   const form = new FormGroup();
+  //
+  //
+  //   return form;
+  //
+  // }
+
   drawGrid(): void {
     this.gridCanvasContext.clearRect(0, 0, 10000, 10000);
     this.gridCanvasContext.strokeStyle = 'black';
@@ -75,7 +117,7 @@ export class GrantTableComponent implements OnInit, AfterViewInit {
     this.gridCanvasContext.beginPath();
     // this.gridCanvasContext.rect(10, 10, 100, 100);
     for (let i = 0; i < 100; i++) {
-      const nextX = this.tableWidth * (i / 10);
+      const nextX = this.tableWidth * i;
       this.gridCanvasContext.moveTo(nextX, 0);
       this.gridCanvasContext.lineTo(nextX, 800);
     }
